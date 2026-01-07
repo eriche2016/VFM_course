0b0737bc40d2a67ad5f72ec9d1f32e5edae0b2ea
eriche2016 <eriche.hust@gmail.com>
2026-01-07 12:15:32 +0800

    add comments

--- run.sh
+++ run.sh
@@ -1,14 +1,26 @@
-
+########################################
 # step 1: modify docs
+########################################
+
+########################################
 # step 2: modify mkdocs.yml if needed
-# remvove existing site directory if exists
-rm -rf site
-# remove existing files in root directory 
+########################################
+########################################
+# step 3: remove existing files in root directory 
 # except docs, mkdocs.yml, .git and run.sh
+########################################
+# step 4: build site and move to root directory
 find . -maxdepth 1 ! -name 'docs' ! -name 'mkdocs.yml' ! -name '.git' ! -name 'run.sh' ! -name '.' -exec rm -rf {} +
-# step 3: build site
+
+#########################################
+# step 5: build site
+##########################################
 mkdocs build
 # mv site to root directory
 mv site/* .
+
+#########################################
+# step 6: clean up
 # remove site directory
+##########################################
 rm -rf site
