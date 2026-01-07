f522eec6438196bb5108e6d964d9bf922962e43e
eriche2016 <eriche.hust@gmail.com>
2026-01-07 12:13:24 +0800

    clean code

--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
+
+# step 1: modify docs
+# step 2: modify mkdocs.yml if needed
+# remvove existing site directory if exists
+rm -rf site
+# remove existing files in root directory 
+# except docs, mkdocs.yml, .git and run.sh
+find . -maxdepth 1 ! -name 'docs' ! -name 'mkdocs.yml' ! -name '.git' ! -name 'run.sh' ! -name '.' -exec rm -rf {} +
+# step 3: build site
 mkdocs build
 # mv site to root directory
 mv site/* .
